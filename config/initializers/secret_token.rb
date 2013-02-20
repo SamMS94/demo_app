3428aad609c28c0b42f1bf79bc6b9d347b12515b
Sam Sussman <samuel.sussman@yale.edu>
2013-02-20 18:34:04 -0500

    Finished but it isnt displaying on localhost. idk how to fix

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DemoApp::Application.config.secret_token = 'ae73ef552217f57446af1ade04f86e097a2efb9cfecc5b91c9c31947ca6a9e688e2a94bc9adf04030f4d1e3ea6127c3c1b3126bbacdf0e9a0eecc713deab3ea7'
